b1067a1c7286bd09332060a399db4611a6c1cf45
muziyi <41556969+hyrzyk92@users.noreply.github.com>
2022-07-20 17:01:31 +0800

    自定义插件

---
+++ a.js
@@ -0,0 +1,26 @@
+
+class APlugin {
+    apply(complier) {
+        complier.hooks.emit.tapAsync('APlugin', (compilation, callback) => {
+            var filelist = 'In this build:\n\n';
+            // 遍历所有编译过的资源文件，
+            // 对于每个文件名称，都添加一行内容。
+            for (var filename in compilation.assets) {
+                filelist += '- ' + filename + '\n';
+            }
+            // 将这个列表作为一个新的文件资源，插入到 webpack 构建中：
+            compilation.assets['filelist.md'] = {
+                source: function() {
+                    return filelist;
+                },
+                size: function() {
+                    return filelist.length;
+                }
+            };
+            callback();
+
+        })
+    }
+}
+
+module.exports = APlugin
