fb4ee8087f414ad6050b6b590365b5283a5fe9f1
muziyi <41556969+hyrzyk92@users.noreply.github.com>
2022-07-20 17:03:27 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
+const APlugin = require('./APlugin.js')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'main.js'
-    }
+    },
+    plugins: [
+        new APlugin()
+    ]
 }
